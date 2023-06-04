1b184e392cf51b25d252e1e3bc815e8e635913d4
wetinx <wetin206@qq.com>
2023-06-04 23:42:05 +0800

    第一次提交

---
+++ src/com/wetin/web/servlet/ServletDemo4.java
@@ -0,0 +1,30 @@
+package com.wetin.web.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @version 1.0.1
+ * @Author: wei tin
+ */
+@WebServlet("/demo4")
+public class ServletDemo4 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println("-----doGet----");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        System.out.println("-----doPost------");
+    }
+}
